b07d572f317f2f902d29bd5d9d722eb348ad1fec
Martin0Eden <dimka17372@gmail.com>
2023-07-05 01:36:59 +0700

    фул маршруты

--- маршрутка онлайн/Controllers/HomeController.cs
+++ маршрутка онлайн/Controllers/HomeController.cs
@@ -139,6 +139,27 @@ namespace маршрутка_онлайн.Controllers
             return View();
         }
 
+        [HttpGet]
+
+        public IActionResult in_cart()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult up_cart()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult del_cart()
+        {
+            return View();
+        }
+
         [HttpPost]
 
         public async Task<IActionResult> Message(message message)
@@ -308,6 +329,46 @@ namespace маршрутка_онлайн.Controllers
             sql_index sqlMes = new sql_index();
 
             if (!string.IsNullOrEmpty(taxi.zag))
+            {
+                await sqlMes.Delete(taxi);
+            }
+
+            return RedirectToAction("Administration");
+        }
+
+        public async Task<IActionResult> in_cart(cart taxi)
+        {
+            sql_cart sqlMes = new sql_cart();
+
+            if (ModelState.IsValid)
+            {
+                await sqlMes.Add(taxi);
+            }
+
+            return RedirectToAction("Administration");
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> up_cart(cart taxi)
+        {
+            sql_cart sqlMes = new sql_cart();
+
+            if (ModelState.IsValid)
+            {
+                await sqlMes.Update(taxi);
+            }
+
+            return RedirectToAction("Administration");
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> del_cart(cart taxi)
+        {
+            sql_cart sqlMes = new sql_cart();
+
+            if (!string.IsNullOrEmpty(taxi.name))
             {
                 await sqlMes.Delete(taxi);
             }
--- маршрутка онлайн/Models/sql/sql_cart.cs
+++ маршрутка онлайн/Models/sql/sql_cart.cs
@@ -41,5 +41,76 @@ namespace маршрутка_онлайн.Models.sql
                 connection.Close();
             }
         }
+        public async Task Add(cart message)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string insertQuery = $"INSERT INTO path (name, price, time_start, time_end, interval, point_start, point_end, full_path, work, color) VALUES (@name, @price, @time_start, @time_end, @interval, @point_start, @point_end, @full_path, @work, @color)";
+                using (SQLiteCommand insertCommand = new SQLiteCommand(insertQuery, connection))
+                {
+                    insertCommand.Parameters.AddWithValue("@name", message.name);
+                    insertCommand.Parameters.AddWithValue("@price", message.price);
+                    insertCommand.Parameters.AddWithValue("@time_start", message.time_start);
+                    insertCommand.Parameters.AddWithValue("@time_end", message.time_end);
+                    insertCommand.Parameters.AddWithValue("@interval", message.interval);
+                    insertCommand.Parameters.AddWithValue("@point_start", message.point_start);
+                    insertCommand.Parameters.AddWithValue("@point_end", message.point_end);
+                    insertCommand.Parameters.AddWithValue("@full_path", message.full_path);
+                    insertCommand.Parameters.AddWithValue("@work", message.work);
+                    insertCommand.Parameters.AddWithValue("@color", message.color);
+                    await insertCommand.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        public async Task Update(cart message)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string updateQuery = @"UPDATE path 
+                      SET  
+                          price = @price, 
+                          time_start = @time_start, 
+                          time_end = @time_end, 
+                          interval = @interval, 
+                          point_start = @point_start, 
+                          point_end = @point_end, 
+                          full_path = @full_path, 
+                          work = @work, 
+                          color = @color 
+                      WHERE name = @name";
+                using (SQLiteCommand updateCommand = new SQLiteCommand(updateQuery, connection))
+                {
+                    updateCommand.Parameters.AddWithValue("@name", message.name);
+                    updateCommand.Parameters.AddWithValue("@price", message.price);
+                    updateCommand.Parameters.AddWithValue("@time_start", message.time_start);
+                    updateCommand.Parameters.AddWithValue("@time_end", message.time_end);
+                    updateCommand.Parameters.AddWithValue("@interval", message.interval);
+                    updateCommand.Parameters.AddWithValue("@point_start", message.point_start);
+                    updateCommand.Parameters.AddWithValue("@point_end", message.point_end);
+                    updateCommand.Parameters.AddWithValue("@full_path", message.full_path);
+                    updateCommand.Parameters.AddWithValue("@work", message.work);
+                    updateCommand.Parameters.AddWithValue("@color", message.color);
+                    await updateCommand.ExecuteNonQueryAsync();
+                }
+            }
+        }
+        public async Task Delete(cart message)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string deleteQuery = $"DELETE FROM path  WHERE name = @name";
+                using (SQLiteCommand deleteCommand = new SQLiteCommand(deleteQuery, connection))
+                {
+                    deleteCommand.Parameters.AddWithValue("@name", message.name);
+                    deleteCommand.ExecuteNonQuery();
+                    await deleteCommand.ExecuteNonQueryAsync();
+                }
+
+            }
+        }
     }
 }
